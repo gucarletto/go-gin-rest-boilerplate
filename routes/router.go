a1b95578804e86bd2d24e93ed4d467d64b1d4fdc
Gustavo Carletto <gucarletto@gmail.com>
2021-09-22 09:22:14 -0300

    Fix routes and added status route

--- routes/router.go
+++ routes/router.go
@@ -5,6 +5,8 @@ import "github.com/gin-gonic/gin"
 func GetRoutesV1(router *gin.Engine) *gin.RouterGroup {
 	v1 := router.Group("v1")
 
+	v1.GET("status", getStatus)
+
 	v1.GET("users/:id")
 	v1.GET("users")
 	v1.POST("users")
@@ -15,4 +17,8 @@ func GetRoutesV1(router *gin.Engine) *gin.RouterGroup {
 	v1.POST("transactions")
 
 	return v1
+}
+
+func getStatus(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "ok"})
 }
--- server/server.go
+++ server/server.go
@@ -1,9 +1,14 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gucarletto/go-gin-rest-boilerplate/routes"
+)
 
 func Start() {
 	router := gin.Default()
+
+	routes.GetRoutesV1(router)
 
 	router.Run(":4242")
 }
